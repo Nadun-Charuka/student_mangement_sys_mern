0653776ae1a13d27b7a9435990043facdf026f20
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-01-18 10:47:42 +0530

    db connect

---
+++ BACKEND/server.js
@@ -0,0 +1,39 @@
+// Importing all the packages 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Configure environment variables
+dotenv.config();
+
+// Initialize the Express app
+const app = express();
+
+// Define the port
+const PORT = process.env.PORT || 8070;
+
+// Enable CORS (Cross-Origin Resource Sharing) for your Express.js app
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection URL from environment variables
+const URL = process.env.MONGODB_URL;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("MongoDB connection successful!");
+})
+.catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port number: ${PORT}`);
+});
